61cdc120f4b95f0f1876a1d010dae3ebd04b50bd
fatimatahir190 <fatimatahirft190@gmail.com>
2026-08-20 02:36:55 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,74 @@
+
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from supabase import create_client
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+url = "https://uhrocmzeksepafmcllin.supabase.co"
+key =  "sb_publishable_TV5r7yzM84ax0vR8eyfXqg_-pXoly39"
+
+supabase = create_client(url, key)
+
+
+class Todo(BaseModel):
+    title: str
+    description: str
+    is_completed: bool
+
+
+@app.post("/todos")
+def create_todo(todo: Todo):
+
+    data = {
+        "title": todo.title,
+        "description": todo.description,
+        "is_completed": todo.is_completed
+    }
+
+    response = supabase.table("todos").insert(data).execute()
+
+    return response.data
+
+
+@app.put("/todos/{todo_id}")
+def update_todo(todo_id: int, todo: Todo):
+
+    response = supabase.table("todos").update({
+        "title": todo.title,
+        "description": todo.description,
+        "is_completed": todo.is_completed
+    }).eq("id", todo_id).execute()
+
+    return response.data
+
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int):
+
+    response = supabase.table("todos").delete().eq(
+        "id", todo_id
+    ).execute()
+
+    return response.data
+
+
+@app.get("/todos")
+def get_todos():
+
+    response = supabase.table("todos").select("*").execute()
+
+    return response.data
